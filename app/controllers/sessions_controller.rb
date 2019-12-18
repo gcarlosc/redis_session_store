616567422f16ce2299ffdb668a4ef338e2f93dea
Giancarlo Solis <gsoliscastillo@gmail.com>
2019-12-17 22:32:51 -0500

    add simple authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      log_in user
+      redirect_to root_url, notice: "Logged in!"
+    else
+      flash.now[:alert] = "Email or password is invalid"
+      render "new"
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
+  end
+end
